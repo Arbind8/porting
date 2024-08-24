105bfcb4561e81e338bb5e355b301485aca32ab6
Arbind8 <71551895+Arbind8@users.noreply.github.com>
2024-08-25 00:29:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+var typed = new Typed (".text", {
+
+strings:["Frontend Developer","Android Developer","Full Stack Web Developer"],
+typeSpeed:120,
+backSpeed:80,
+delaySpeed:1000,
+loop: true,
+
+});
+
+var typed = new Typed (".name",{
+
+
+})
+
+document.getElementById("form").reset();
